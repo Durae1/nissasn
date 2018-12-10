ea737fe85917d745ba9e6c5ce7bfa1a9b7225bce
Durae1 <43078020+Durae1@users.noreply.github.com>
2018-12-10 08:38:54 -0500

    Add files via upload

---
+++ carLand.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author lewic
+ */
+public class carLand 
+        public static void main(String[] args){
+
+car nissanS14 = new car(); // creates obj of class car
+nissanS14.passengers = 5;
+nissanS14.horsePower = 217;
+nissanS14.fuelCapacity = 16;
+nissanS14.mlesPerGallon = 22;
+nissanS14.maxSpeed = 119;  
+nissanS14.engine = SR20DET;
+
+
+
+  
+
+
+    
+    
+    
+}   
+
+system.out.println("Nissan S14 passengers: " + nissanS14.passengers);
---
+++ nissan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author lewic
+ */
+public class nissan {
+class car { 
+    public int passengers;
+    //amount of passengerts tis vehicle can hold
+    public int horsePower;
+    //amount of stock horsepower
+    public double fuelCapacity;
+    //max fuel level
+    public double milesPerGallon;
+    //the mpg of the vehicle
+    public int maxSpeed;
+    //the max speed of the vehicle
+    public String engine;
+    //the motor in the vehicle
+}   
+    public static void main(String[]args){ 
+        car firstCar;
+        firstCar = new nissanS14();
+        
+    System.out.println("Nissan S14 passengers: " + nissanS14.passengers);
+    
+    }
